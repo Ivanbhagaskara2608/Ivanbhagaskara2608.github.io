d296b66ab3910037fad3cfc911346aa9c342bffc
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2023-04-04 11:22:09 +0700

    edit map foto

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menuVisible = false;
-//Función que oculta o muestra el menu
+//Function that hides or shows the menu
 function showMenu(){
     if(menuVisible){
         document.getElementById("nav").classList ="";
@@ -11,31 +11,31 @@ function showMenu(){
 }
 
 function selectMenu(){
-    //oculto el menu una vez que selecciono una opcion
+    //Hide the menu once I select an option
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
+//Function that applies the animations of the skills
+function effectSkills(){
     var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progress");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("python");
-        habilidades[3].classList.add("kotlin");
-        habilidades[4].classList.add("coreldraw");
-        habilidades[5].classList.add("communication");
-        habilidades[6].classList.add("teamwork");
-        habilidades[7].classList.add("creativity");
-        habilidades[8].classList.add("dedication");
-        habilidades[9].classList.add("project");
+    var distance_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distance_skills >= 300){
+        let skillsClass = document.getElementsByClassName("progress");
+        skillsClass[0].classList.add("javascript");
+        skillsClass[1].classList.add("htmlcss");
+        skillsClass[2].classList.add("python");
+        skillsClass[3].classList.add("kotlin");
+        skillsClass[4].classList.add("coreldraw");
+        skillsClass[5].classList.add("communication");
+        skillsClass[6].classList.add("teamwork");
+        skillsClass[7].classList.add("creativity");
+        skillsClass[8].classList.add("dedication");
+        skillsClass[9].classList.add("project");
     }
 }
 
 
-//detecto el scrolling para aplicar la animacion de la bar de habilidades
+//detect the scrolling to apply the animation of the skill bar
 window.onscroll = function(){
-    efectoHabilidades();
+    effectSkills();
 } 
